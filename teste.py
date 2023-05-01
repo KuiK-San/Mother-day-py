fe4ac196ba5eedcb76ec4d5ddaa4507f046d306f
Guilherme Casagrande <thegui4000@gmail.com>
2023-05-01 15:35:20 -0300

    abrindo direto no psd

--- teste.py
+++ teste.py
@@ -1,16 +1,13 @@
 import photoshop.api as ps
+from photoshop import Session
 import pyautogui
 import sys, os
+from PIL import Image
+from os import walk, path
 from time import sleep
 sys.path.insert(0, './src')
-from photoshopy import Photoshopy
 
-app = Photoshopy()
-pyautogui.alert(title='Photoshop', text='Por favor Clique no photoshop antes de prosseguir', button='OK')
-sleep(1)
-pyautogui.hotkey('ctrl', 'o')
-pyautogui.alert(title='Photoshop', text='Por favor selecione o arquivo .PSD', button='OK')
-sleep(10)
-para = pyautogui.confirm(title='Photoshop', text='Por favor certifiquesse que o Photoshop está aberto no arquivo cartaz_mae.psd\nA partir do momento que você pressionar OK solte o mouse e teclado até o fim da operação', buttons=['OK', 'Cancelar'])
-if para == 'Cancelar':
-    exit();
+prompt = pyautogui.prompt(text='digite o caminhho do arquivo psd', title='photoshop', default='E:/Users/thegu/Documents/ps_py/resources/cartaz_mae.psd')
+caminho = os.path.abspath(prompt);
+app = ps.Application()
+app.load(caminho)
