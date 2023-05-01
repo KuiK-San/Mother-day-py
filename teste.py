05dca66985622edb0631b6c9e1bb07050c6c95b1
Guilherme Casagrande <thegui4000@gmail.com>
2023-05-01 19:36:00 -0300

    mais algumas alterações

--- teste.py
+++ teste.py
@@ -1,5 +1,6 @@
 import photoshop.api as ps
 from photoshop import Session
+from tempfile import mkdtemp
 import pyautogui
 import sys, os
 from PIL import Image
@@ -11,8 +12,23 @@ prompt = pyautogui.prompt(text='digite o caminhho do arquivo psd', title='photos
 caminho = os.path.abspath(prompt);
 app = ps.Application()
 app.load(caminho)
-with Session() as ps:
-    doc = ps.active_document
-    layername = 'texto' + "1"
-    text_layer = doc.artLayers.getByName(layername)
-    text_layer.textItem.contents = "Nome da mãe"
+with open('nomes_e_path.txt', 'r') as arquivo:
+    index = 0
+    index2 = 1
+    for linha in arquivo:
+        nome = linha.split(',')[0]
+        with Session() as ps:
+            doc = ps.active_document
+            layername = 'texto' + str(index + 1)
+            text_layer = doc.artLayers.getByName(layername)
+            text_layer.textItem.contents = str(nome)
+        index += 1
+        if index % 4 == 0:
+            pyautogui.alert(text='cansei')
+            index = 0
+            options = ps.JPEGSaveOptions(quality=5)
+            nomeimg = "imagem" + str(index2)
+            jpg_file = os.path.join("E:/Users/thegu/Documents/ps_py/resources/files", nomeimg + ".jpg")
+            doc.saveAs(jpg_file, options, asCopy=True)
+            os.startfile(jpg_file)
+            index2 += 1
