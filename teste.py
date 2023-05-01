ff450c5ec4a9a9e5aceb02bdfabed85d324d8cf3
Guilherme Casagrande <thegui4000@gmail.com>
2023-05-01 20:22:14 -0300

    trocando imagem (tem que concertar melhor)

--- teste.py
+++ teste.py
@@ -8,6 +8,7 @@ from os import walk, path
 from time import sleep
 sys.path.insert(0, './src')
 
+
 prompt = pyautogui.prompt(text='digite o caminhho do arquivo psd', title='photoshop', default='E:/Users/thegu/Documents/ps_py/resources/cartaz_mae.psd')
 caminho = os.path.abspath(prompt);
 app = ps.Application()
@@ -17,6 +18,36 @@ with open('nomes_e_path.txt', 'r') as arquivo:
     index2 = 1
     for linha in arquivo:
         nome = linha.split(',')[0]
+        origem_img = linha.split(',')[1]
+        img = linha.split(',')[2]
+        match index:
+            case 0:
+                pyautogui.click(1029,596, duration=0.5)
+                sleep(0.2)
+                pyautogui.doubleClick(1029,596, duration=0.5)
+            case 1:
+                pyautogui.doubleClick(1034,560, duration=0.5)
+            case 2:
+                pyautogui.doubleClick(1036,517, duration=0.5)
+            case 3:
+                pyautogui.doubleClick(1039,475, duration=0.5)
+        pyautogui.click(68,17, duration=0.5)
+        pyautogui.click(60,405, duration=0.5)
+        pyautogui.click(125,49)
+        pyautogui.write(origem_img)
+        pyautogui.press('enter')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.press('tab')
+        pyautogui.write(img)
+        pyautogui.press('enter')
+        pyautogui.press('enter')
+        pyautogui.hotkey('ctrl', 's')
+        pyautogui.hotkey('ctrl', 'w')
+        
         with Session() as ps:
             doc = ps.active_document
             layername = 'texto' + str(index + 1)
