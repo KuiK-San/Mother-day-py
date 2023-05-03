36100b9b7accf1add89490bf1607c563c15d2c18
Guilherme Casagrande <thegui4000@gmail.com>
2023-05-03 19:24:50 -0300

    finalizado

--- resources/images/red_img.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-img = Image.open('c:/Users/thegu/Documentos/Mother-day-py/resources/images/mae1.png')
-img_resized = img.resize((240, 240))
-img_resized.save('c:/Users/thegu/Documentos/Mother-day-py/resources/images/oi.png')
